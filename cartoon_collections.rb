486b02f5e6e46a7f8ade7b5bd916fe1c701410f6
jinook929 <jinook929@gmail.com>
2021-01-13 20:29:20 -0500

    commit tree b8463d8d2af1e6e3c930b11ced0de8528d6ca798

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(arr) # code an argument here
   # Your code here
+  arr.each_with_index {|name, index|
+    puts "#{index + 1}. #{name}"
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr) # code an argument here
   # Your code here
+  arr.collect {|e|
+    "#{e.capitalize}!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arr) # code an argument here
   # Your code here
+  arr.any?{|e|
+    e.length > 4
+  }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each {|e|
+    if arr.include?(e)
+      return e
+    end
+  }
+  nil
 end
